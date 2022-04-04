ca7346fe459541ad8134edb17ab638d90a1bc24a
Deepanshu Shukla <deepanshushukla@Deepanshus-MacBook-Pro.local>
2022-04-05 00:40:03 +0530

    Fix home page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,42 @@
 import React from "react";
 import "./Footer.scss";
-import { FacebookFilled ,PhoneFilled , TwitterSquareFilled , MailFilled ,LinkedinFilled} from '@ant-design/icons';
-const Footer2 = ({
+import {
+  FacebookFilled,
+  PhoneFilled,
+  TwitterSquareFilled,
+  MailFilled,
+  LinkedinFilled,
+} from "@ant-design/icons";
 
-}) => {
+const SocialAppFooter = ({}) => {
   return (
-<div className="footer-wrapper">
-  <div>
-<a  href="www.facebook.com"><FacebookFilled /></a>
-<a  href="www.twitter.com"><TwitterSquareFilled /></a>
-<a href="www.linekdin.com"><LinkedinFilled /></a>
-</div>
-<div className="">  
-<a  href="www.gmail.com"><MailFilled /></a>
-<a href="tel:+91-7838116024"><PhoneFilled /></a>
-</div>
-</div>
-  )
+    <div className="footer-wrapper">
+      <div>
+        <a
+          target="_blank"
+          href="https://www.facebook.com/profile.php?id=100007840425885"
+        >
+          <FacebookFilled />
+        </a>
+        <a target="_blank" href="https://twitter.com/deeps1790">
+          <TwitterSquareFilled />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/deepanshu-shukla-48227062/"
+        >
+          <LinkedinFilled />
+        </a>
+      </div>
+      <div className="">
+        <a target="_blank" href="mailto:deeps1790@gmail.com">
+          <MailFilled />
+        </a>
+        <a target="_blank" href="tel:+91-7838116024">
+          <PhoneFilled />
+        </a>
+      </div>
+    </div>
+  );
 };
-export default Footer2;
+export default SocialAppFooter;
